27396600c2e774b892cc508f6e763561143fe592
陈天林 <1256860196@qq.com>
2022-07-12 17:25:39 +0800

    更新

---
+++ logger.js
@@ -0,0 +1,19 @@
+//  function Print() {
+//   var LODOP = getLodop();
+//   LODOP.PRINT_INIT("打印控件功能演示_Lodop功能_打印纸张设置");
+//   LODOP.SET_PRINT_PAGESIZE(1, 0, 0, "A4");
+//   LODOP.ADD_PRINT_HTM(0, 0, "100%", "100%", document.getElementById("print").innerHTML);
+//   LODOP.PREVIEW();
+
+// title: '打印预览',
+// width: '100%',
+// height: '100%',
+// border: '1px solid #000',
+// background: '#fff',
+// fontSize: '14px',
+// fontFamily: 'sans-serif',
+// header: '',
+// footer: '',
+// beforePrint: function() {},
+// afterPrint: function() {}
+// }, options)
